865a869bc0f61439464ff92c21f0c306d44898e7
Denis <payar3282@gmail.com>
2022-07-11 16:40:01 +0300

    Cosmetic changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Weather from "./components/weatherComponent/Weather";
-
-const FEATURED_API = (prop) => `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${prop}&appid=522f7ec766b55c89fccbc47a4e7a72c0`
-
+import React, { useRef } from "react"
+import { useState, useEffect } from "react"
+import Weather from "./components/weatherComponent/Weather"
 
 function App() {
+	const [weather, setWeather] = useState("Kyiv")
 
-  const [weather, setWeather] = useState('Kyiv');
+	const test = useRef(false)
+	useEffect(() => {
+		if (test.current === false) {
+			getWeather()
+		}
+		return () => {
+			test.current = true
+		}
+	}, [weather])
 
+	const getWeather = async () => {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${weather}&appid=522f7ec766b55c89fccbc47a4e7a72c0`
+		)
+		const result = await response.json()
+		const newWeather = {
+			name: result.name,
+			country: result.sys.country,
+			mainTemp: Math.round(result.main.temp),
+			feelsLike: Math.round(result.main.feels_like),
+			weatherStatus: result.weather[0].main,
+			weatherIcon: `https://openweathermap.org/img/w/${result.weather[0].icon}.png`,
+		}
+		setWeather(newWeather)
+	}
 
-  useEffect(() => {
-    getWeather(FEATURED_API(weather))
-  }, []);
-
-
-  const getWeather = async (API) => {
-      const response = await fetch(API)
-      const result = await response.json()
-      const newWeather = {
-        name: result.name,
-        country: result.sys.country,
-        mainTemp: Math.round(result.main.temp),
-        feelsLike: Math.round(result.main.feels_like),
-        weatherStatus: result.weather[0].main,
-        weatherIcon: `https://openweathermap.org/img/w/${result.weather[0].icon}.png`
-      }
-      setWeather(newWeather)
-  }
-
-  return (
-    <div className="App">
-      <main className="weather-container">
-        <Weather {...weather} />
-      </main>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<main className='weather-container'>
+				<Weather {...weather} />
+			</main>
+			<button onClick={() => setWeather("Moscow")}>click</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
